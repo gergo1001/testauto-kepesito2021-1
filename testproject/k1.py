d53f7e75150cb7acfba58951dfc193227141176b
Nagy Gergely <nagygergely.levelek@gmail.com>
2021-09-06 14:41:30 +0200

    mentés

--- testproject/k1.py
+++ testproject/k1.py
@@ -1,7 +1,5 @@
-
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-
 
 # In order for ChromeDriverManager to work you must pip install it in your own environment.
 driver = webdriver.Chrome(ChromeDriverManager().install())
@@ -27,33 +25,38 @@ def get_inputmezo(input_id):
 def get_result_text(result_id):
     return driver.find_element_by_id(result_id).text
 
+
 # láthatóságot vizsgálná
 def get_result_text_visible(result_id):
-    s = driver.find_element_by_id(results_visible_id).get_property("style")
-    if s == "inline":
+    s = driver.find_element_by_id(results_visible_id).get_attribute("style")
+    if s == "display: inline;":
         return True
     else:
         return False
 
-#lekéri az induló állapotot és azt ellenőrzi
-def test_TC01():
 
+# lekéri az induló állapotot és azt ellenőrzi
+def test_TC01():
     assert get_inputmezo(input_a_id) == ""
     assert get_inputmezo(input_b_id) == ""
     assert get_result_text(result_id) == ""
     assert get_result_text_visible(results_visible_id) is False
 
-#valós értékekkel tesztel
+
+# valós értékekkel tesztel
 def test_TC02():
     fill_inputmezo(input_a_id, "2")
     fill_inputmezo(input_b_id, "3")
     driver.find_element_by_id(submit_id).click()
     assert get_result_text(result_id) == "10"
+    assert get_result_text_visible(results_visible_id) is True
 
-#üress mezőkkel tesztel
+
+# üress mezőkkel tesztel
 def test_TC03():
     fill_inputmezo(input_a_id, "")
     fill_inputmezo(input_b_id, "")
     driver.find_element_by_id(submit_id).click()
     assert get_result_text(result_id) == "NaN"
-    driver.close()
+    assert get_result_text_visible(results_visible_id) is True
+
